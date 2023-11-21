a2545db32036e0c550c6f7b42acdd557fb8adf10
Issam Abou <issamabou77@gmail.com>
2023-11-21 21:27:44 +0000

    finished the logic for the darkmode button toggle

--- src/components/DarkMode/dark-mode.component.jsx
+++ src/components/DarkMode/dark-mode.component.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import './dark-mode.styles.css'
+import './dark-mode.styles.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
@@ -9,6 +9,11 @@ const DarkModeToggle = () => {
 
     const handleDarkModeToggle = () => {
         setIsDarkMode(!isDarkMode)
+        document.body.classList.toggle('dark-mode')
+        const navbar = document.querySelector('nav')
+        if (navbar) {
+            navbar.classList.toggle('dark-mode')   
+        }
         console.log('toggle-icon is:', isDarkMode)
     }
 
